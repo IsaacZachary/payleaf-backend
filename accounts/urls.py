b80d938f69d3e02d0ef8a8480192d622b8a5b639
IsaacZachary <isaaczachary18@gmail.com>
2026-04-17 17:12:08 +0300

    feat: configure authentication and password management URL patterns for accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+    path('csrf', views.CsrfCookieView.as_view(), name='csrf'),
     path('login', views.LoginView.as_view(), name='login'),
     path('logout', views.LogoutView.as_view(), name='logout'),
     path('me', views.MeView.as_view(), name='me'),
